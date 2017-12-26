df59c7cb1fdfac9683b66b8387710364931fa36b
John Kotz <john.lyme@mac.com>
2017-12-26 16:12:42 -0500

    Adding week 3 activity

---
+++ Week3/activity.py
@@ -0,0 +1,13 @@
+# This weeks activity will be a password checker
+
+# Enter the password
+PASSWORD = "password123"
+
+# The guess
+guess = input("Password: ")
+
+# Write a loop that checks the password
+# When the guess is wrong, ask again
+
+
+# When right, tell the user
