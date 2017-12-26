e577fe849f0ab0c8b47cb9faaf2d4982fc177a88
John Kotz <john.lyme@mac.com>
2017-12-26 17:29:21 -0500

    Moving activity1 from Week 5 to Week 4

---
+++ Week4/activity1.py
@@ -0,0 +1,35 @@
+from Week4.drawing_activity import *
+
+"""
+Activity Description:
+Before diving into drawing, it's important to understand that the order which we draw onto a canvas matters.
+For example, in order to draw a door at the front of a house, we would first draw the house, and then the door.
+Drawing the door then the house would result in the house covering up the door; therefore the door wouldn't appear on
+the canvas. For the activity below, there are nine images that must be drawn in the correct order to reveal a simple
+scene. Each image can be drawn by calling its corresponding 'draw' function. Please call and rearrange the nine 'draw'
+functions to paint the scene.
+"""
+
+# ==============================
+# ===== CODE ACTIVITY HERE =====
+# ==============================
+
+# Functions to call:
+
+draw_sky()
+draw_sun()
+draw_dark_clouds()
+draw_light_clouds()
+draw_mountains()
+draw_grass()
+draw_road()
+draw_bush()
+draw_flowers()
+
+
+# ============================
+# === END OF CODE ACTIVITY ===
+# ============================
+
+load_images()
+start_graphics(main_draw, width=WIDTH, height=HEIGHT)
---
+++ Week4/activity2.py
@@ -0,0 +1,8 @@
+"""
+Week 4 Activity: Green Eggs and Ham
+-----------------------------------
+Draw green eggs and ham on a plate
+"""
+
+from cs1lib import *
+
